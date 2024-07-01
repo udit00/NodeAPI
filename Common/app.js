570f91be7ac2b95e0c5098d292aa151e588ec9bd
root <root@srv554627.hstgr.cloud>
2024-07-01 15:36:13 +0000

    Ip address added to login and logs

--- Common/app.js
+++ Common/app.js
@@ -3,6 +3,16 @@ const APPNAME = {
     todo: "todo"
 }
 
+export function isBlank(check) {
+    return (check == undefined || check == null || check == ''); 
+}
+
 export function getAppDBName(app){
     return (APPNAME[`${app}`] == undefined) ? '404' : APPNAME[`${app}`];
+}
+
+export function getUserIP(req) {
+    const ip = req.socket.remoteAddress;
+    const arr = ip.split(':');
+    return arr[arr.length - 1] ?? ip;
 }
